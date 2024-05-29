509f25e7e191284d248bef66db62738c1544b1cc
ryohei.kobayashi <ryohei.kobayashi@okadanet.org>
2024-05-30 02:19:58 +0900

    add recog human

--- deepface-ros-docker/face_recognizer/scripts/face_recog_server.py
+++ deepface-ros-docker/face_recognizer/scripts/face_recog_server.py
@@ -4,12 +4,6 @@
 import os
 import traceback
 
-#import tensorflow as tf
-#print('###########################################')
-#tf.test.is_gpu_available()
-#print('###########################################')
-
-
 from deepface import DeepFace
 from retinaface import RetinaFace
 
@@ -21,9 +15,8 @@ import rospy
 
 from geometry_msgs.msg import Point
 from sensor_msgs.msg import PointCloud2, Image
+
 from rospkg import RosPack
-
-
 rospack = RosPack()
 known_face_image_path = rospack.get_path('face_recognizer')+'/io/face_images/known_faces'
 detected_face_image_path = rospack.get_path('face_recognizer')+'/io/face_images/detected_faces'
@@ -44,6 +37,7 @@ class FaceRecogServer():
     
     def callback(self, image):
 
+        #try:
         msg2img = self.bridge.imgmsg_to_cv2(image, "bgr8")
         faces = RetinaFace.detect_faces(msg2img)
 
@@ -58,14 +52,25 @@ class FaceRecogServer():
                 bbox = cv2.rectangle(msg2img, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 crop_face = bbox[y1:y2, x1:x2]
                 
-                cv2.imshow('RETINA FACE DETECTION NODE BY RYOHEISOFT', bbox)
-                cv2.imshow('DETECTED FACE', crop_face)
-                cv2.waitKey(50)
 
                 #save tmp image for matching
                 temp_face_path = os.path.join(detected_face_image_path, 'temp_face.jpg')
                 cv2.imwrite(temp_face_path, crop_face)
 
+                if not os.path.exists(temp_face_path):
+                    print(f"Error: Temporary face image was not saved at {temp_face_path}")
+                    continue
+
+
+                try:
+                    test_img = cv2.imread(temp_face_path)
+                    if test_img is None:
+                        print(f"Error: Could not read the saved temporary face image at {temp_face_path}")
+                        continue
+                except Exception as e:
+                        continue
+
+                
                 #face recognition using with deepface
                 known_face_detected = False
                 for file in os.listdir(known_face_image_path):
@@ -73,26 +78,36 @@ class FaceRecogServer():
                     if os.path.isfile(known_face_path) and file.lower().endswith(('jpg', 'jpeg', 'png')):
                         
                         try:
-                            result = DeepFace.verify(temp_face_path, known_face_path)
-                            print(known_face_path)
-                            if result["verified"]:
-                                known_face_detected = True
-                                print(f"Match found with {file}")
+                            #face recognition
+                            recog = DeepFace.find(
+                              img_path = temp_face_path,
+                              db_path = known_face_image_path, 
+                              model_name = 'DeepFace',
+                              enforce_detection=False
+                            )
+                            if recog:
+                                known_face_detect = True
+                                match_ = file
+                                rospy.loginfo(f"match found with {file}")
                                 break
+
+                            else:
+                                rospy.loginfo(f"No match found")
+
                         except Exception as e:
                             print(f"Error processing {file}: {e}")
+                            known_face_detect = False
+                            import traceback
+                            traceback.print_exc()
 
-                if os.path.exists(temp_face_path):
-                    os.remove(temp_face_path)
+                match_file, _ = os.path.splitext(file)
+                with_label = cv2.putText(bbox, match_file, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                cv2.imshow('FACE RECOGNITION NODE BY RYOHEISOFT', with_label)
+                cv2.imshow('DETECTED FACE', crop_face)
+                cv2.waitKey(50)
 
-                if not known_face_detected:
-                    print("No match found for the detected face.")
-                
-
-        
-
-                
-
+            if os.path.exists(temp_face_path):
+                os.remove(temp_face_path)
 
 if __name__ == '__main__':
    try:
