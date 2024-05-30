2d54cf9b90b0bfaa3f67e0cbfbc2556b9bff74b4
clucker357 <102196920+clucker357@users.noreply.github.com>
2024-05-30 15:23:21 +0900

    [fix]rgb->bgr

--- sign_detection/scripts/sign_detection.py
+++ sign_detection/scripts/sign_detection.py
@@ -4,9 +4,9 @@ from cv_bridge import CvBridge
 import cv2
 
 def edge_detection(image):
-  image = Image.fromarray(image)
-  image = image.convert('L')
-  image = np.array(image)
+  bridge = CvBridge()
+  image = bridge.imgmsg_to_cv2(image, "bgr8")
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
   result1=0
   result2=0
   for i in range(0, image.shape[0]-2, 3):
@@ -25,13 +25,13 @@ def sign_detection():
     pub = rospy.Publisher("/chatter/Int64", Int64, queue_size=1)
     bridge = CvBridge()
     image = bridge.imgmsg_to_cv2(image, "bgr8")
-    image = cv2.resize(image, (160, 90, 4))
+    image = cv2.resize(image, (160, 90, 3))
     color = [0, 0, 0]
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             color += check_data[i, j, :]
     # 黄色を検知した場合
-    if(0.92 < color[0] / color[1] < 1.08 and 1.28 < color[1] / color[2] and 1.28 < color[0] / color[2]):
+    if(0.92 < color[3] / color[1] < 1.08 and 1.28 < color[1] / color[0] and 1.28 < color[3] / color[0]):
         if(1. < edge_detection(image)):
             # 上り坂
             rospy.loginfo("found ascent.")
@@ -41,7 +41,7 @@ def sign_detection():
             rospy.loginfo(3)
             pub.publish(3)
     # 赤を検知した場合
-    if(1.3 < color[0] / color[1] and 0.92 <color[1] / color[2] < 1.08 and 1.3 < color[0] / color[2]):
+    if(1.3 < color[2] / color[1] and 0.92 <color[1] / color[0] < 1.08 and 1.3 < color[2] / color[0]):
         rospy.loginfo("found stop.")
         pub.publish(1)
 if __name__ == '__main__':
